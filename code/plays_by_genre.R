44432e3a17a81c6817858aa2f0d7389d76d640de
mzucc-hi <matteojzucchi@me.com>
2023-02-05 20:03:35 +0000

    plays-by-genre bar chart

--- code/plays_by_genre.R
+++ code/plays_by_genre.R
@@ -11,6 +11,7 @@ library(ggsci)
 library(shadowtext)
 library(ggpubr)
 library(ggthemes)
+library(glue)
 
 # Exploration
 
@@ -56,15 +57,41 @@ genres_jan2023 <- plays_jan2023 %>%
 
 # Visualisation
 
+## Total plays by genre copy to add as data source for labelling
+
+genres_labels <- clean_genres_jan2023 %>% 
+  mutate(y = (total_plays + 13))
+
 clean_genres_jan2023 %>% 
   ggplot() +
-    geom_col(aes(x = reorder(genre, -total_plays), y = total_plays, fill = genre),
+    geom_col(aes(x = reorder(genre, total_plays), y = total_plays, fill = total_plays),
+             colour = "black",
+             lwd = 0.1,
              show.legend = F,
-             width = 0.6) +
+             width = 0.6,
+             alpha = 0.65) +
+    coord_flip() +
+    labs(title = "Total Plays by Genre — Jan 2023",
+         subtitle = "Apple Music Song Library",
+         y = "",
+         x = "",
+         caption = glue(R.version.string, ", Matteo Zucchi")) +
+    geom_hline(yintercept = 0, size = 0.2) +
+    scale_y_continuous(expand = c(0, 10), limits = c(0, 900)) +
+    geom_text(data = genres_labels,
+              aes(x = reorder(genre, total_plays), y = y, label = total_plays),
+              nudge_y = 25,
+              colour = "black",
+              size = 3) +
+    scale_fill_viridis_c(option = "D", direction = -1) +
     theme_pubr() +
     theme(
       text = element_text(size = 9),
-      plot.subtitle = element_text(size = 11, margin = margin(t = 3, b = 6, unit = "pt")),
+      plot.title = element_text(size = 14, face = "bold"),
+      plot.subtitle = element_text(size = 11, margin = margin(b = 6, unit = "pt")),
       plot.caption = element_text(size = 8, hjust = 0, margin = margin(t = 10, unit = "pt")),
+      axis.ticks = element_blank(),
+      axis.line = element_blank(),
+      axis.text.x = element_blank()
     )
   
